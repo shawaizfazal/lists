147c4cccae32f62261ab0a5809da6b539060908b
shawaizfazal <shazmaxfazal@gmail.com>
2026-07-27 13:10:03 +0500

    tuples

---
+++ activity3.py
@@ -0,0 +1,11 @@
+tuplex=("tuple",False,3.2,1)
+print(tuplex)
+tuplex=(4,6,2,8,3,1)
+print(tuplex)
+tuplex=tuplex + (9,)
+print(tuplex)
+tuple1=(50,10,60,70,50)
+print(tuple1.count(50))
+tuplex=(2,3,4,5,6,7,8,9,1)
+_slice=tuplex[3:5]
+print(_slice)
